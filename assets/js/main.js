5ef8f37c3d039569a9a5fb3b0a0ecc3799c42653
João Ladislau <126190669+Jladislauu@users.noreply.github.com>
2025-07-02 17:17:48 -0300

    tirando efeito scroll js

--- assets/js/main.js
+++ assets/js/main.js
@@ -62,14 +62,14 @@
   /**
    * Animation on scroll function and init
    */
-  function aosInit() {
-    AOS.init({
-      duration: 600,
-      easing: 'ease-in-out',
-      once: true,
-      mirror: false
-    });
-  }
+  // function aosInit() {
+  //   AOS.init({
+  //     duration: 600,
+  //     easing: 'ease-in-out',
+  //     once: true,
+  //     mirror: false
+  //   });
+  // }
   window.addEventListener('load', aosInit);
 
   /**
