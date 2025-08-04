46bfa28f66b33d34e90859c972c7c82dde1758e4
João Ladislau <126190669+Jladislauu@users.noreply.github.com>
2025-08-04 12:35:05 -0300

    tirando  Alex Tselegidis do footer back end

--- agenda/application/views/components/backend_footer.php
+++ agenda/application/views/components/backend_footer.php
@@ -14,27 +14,7 @@
         <span>v<?= config('version') ?></span>
     </div>
 
-    <div class="mb-3 me-lg-5 mb-lg-0">
-        <img class="me-1" src="<?= base_url('assets/img/alextselegidis-logo-16x16.png') ?>" alt="Alex Tselegidis Logo">
-
-        <a href="https://alextselegidis.com">Alex Tselegidis</a>
-
-        &copy; <?= date('Y') ?> - Software Development
-    </div>
-
-    <div class="mb-3 me-lg-5 mb-lg-0">
-        <?= lang('licensed_under') ?>
-        <a href="https://www.gnu.org/licenses/gpl-3.0.en.html">
-            GPL-3.0
-        </a>
-    </div>
-
-    <div class="mb-3 me-lg-5 mb-lg-0">
-        <span id="select-language" class="badge bg-dark">
-            <i class="fas fa-language me-2"></i>
-        	<?= ucfirst(config('language')) ?>
-        </span>
-    </div>
+    
 
     <div class="mb-3 me-lg-5 mb-lg-0">
         <a href="<?= site_url('appointments') ?>">
